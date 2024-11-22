59102bc19a60bbfe990ca324f01898aa9ad8cdba
GusXKern <guskern@gmail.com>
2024-11-22 18:32:33 -0500

    sql changes

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -7,7 +7,7 @@ Q: What are the top skills by salary?
 
 SELECT 
     skills,
-    ROUND(AVG(salary_year_avg),0)
+    ROUND(AVG(salary_year_avg),0) AS average_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
